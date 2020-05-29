16eb467f2b0bd71b9156bb79e5b9b633fbdbb335
JMo <jmo@qaperture.com>
2020-05-28 21:25:18 -0700

    adding no_ssl message support per https://www.postgresql.org/docs/13/protocol-flow.html#id-1.10.5.7.11

---
+++ no_ssl.go
@@ -0,0 +1,34 @@
+package pgproto3
+
+import (
+	"encoding/json"
+)
+
+type NoSSL struct{}
+
+// Backend identifies this message as sendable by the PostgreSQL backend.
+func (*NoSSL) Backend() {}
+
+// Decode decodes src into dst. src must contain the complete message with the exception of the initial 1 byte message
+// type identifier and 4 byte message length.
+func (dst *NoSSL) Decode(src []byte) error {
+	if len(src) != 0 {
+		return &invalidMessageLenErr{messageType: "NoSSL", expectedLen: 0, actualLen: len(src)}
+	}
+
+	return nil
+}
+
+// Encode encodes src into dst. dst will include the 1 byte message type identifier and the 4 byte message length.
+func (src *NoSSL) Encode(dst []byte) []byte {
+	return append(dst, '3', 0, 0, 0, 4)
+}
+
+// MarshalJSON implements encoding/json.Marshaler.
+func (src NoSSL) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Type string
+	}{
+		Type: "NoSSL",
+	})
+}
