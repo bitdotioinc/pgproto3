d8d698e2d8aca3d7dab72b64d21ea13d7516c87c
JMo <jmo@qaperture.com>
2020-05-31 15:55:08 -0700

    more bug fixing

--- no_ssl.go
+++ no_ssl.go
@@ -10,7 +10,6 @@ type NoSSL struct{}
 func (*NoSSL) Backend() {}
 
 // Decode decodes src into dst. src must contain the complete message with the exception of the initial 1 byte message
-// type identifier and 4 byte message length.
 func (dst *NoSSL) Decode(src []byte) error {
 	if len(src) != 0 {
 		return &invalidMessageLenErr{messageType: "NoSSL", expectedLen: 0, actualLen: len(src)}
@@ -19,9 +18,9 @@ func (dst *NoSSL) Decode(src []byte) error {
 	return nil
 }
 
-// Encode encodes src into dst. dst will include the 1 byte message type identifier and the 4 byte message length.
+// Encode encodes src into dst. dst should just be one byte
 func (src *NoSSL) Encode(dst []byte) []byte {
-	return append(dst, 'N', 0, 0, 0, 4)
+	return append(dst, 'N')
 }
 
 // MarshalJSON implements encoding/json.Marshaler.
